03c945c1c42354658e83a896d746ad2fb6dd2e1a
Nageenask0786 <nageenask1417@gmail.com>
2023-10-06 06:46:23 +0000

    first commit

--- src/components/BannerCardItem/index.js
+++ src/components/BannerCardItem/index.js
@@ -1,13 +1,18 @@
+import './index.css'
+
 // Write your code here.
+
 const CardList = props => {
-  const bannerList = props
+  const {bannerList} = props
   const {headerText, description, className} = bannerList
   return (
-    <li className={`${className} banner-card`}>
+    <li className={className}>
       <div>
         <h1 className="heading">{headerText}</h1>
         <p className="description">{description}</p>
-        <button className="show-more-btn">Show More</button>
+        <button className="show-more-btn" type="button">
+          Show More
+        </button>
       </div>
     </li>
   )
